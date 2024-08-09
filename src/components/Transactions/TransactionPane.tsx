45a749a94ffb39a1ba4b18cc109ec6d088026045
Animesh Tiwary <63429296+meshtiwa@users.noreply.github.com>
2024-08-09 06:35:11 +0000

    last bug fixed

--- src/components/Transactions/TransactionPane.tsx
+++ src/components/Transactions/TransactionPane.tsx
@@ -1,13 +1,15 @@
-import { useState } from "react"
-import { InputCheckbox } from "../InputCheckbox"
-import { TransactionPaneComponent } from "./types"
+import { useState } from "react";
+import { useCustomFetch } from "src/hooks/useCustomFetch";
+import { InputCheckbox } from "../InputCheckbox";
+import { TransactionPaneComponent } from "./types";
 
 export const TransactionPane: TransactionPaneComponent = ({
   transaction,
   loading,
   setTransactionApproval: consumerSetTransactionApproval,
 }) => {
-  const [approved, setApproved] = useState(transaction.approved)
+  const [approved, setApproved] = useState(transaction.approved);
+  const { clearCache } = useCustomFetch();
 
   return (
     <div className="RampPane">
@@ -15,7 +17,8 @@ export const TransactionPane: TransactionPaneComponent = ({
         <p className="RampText">{transaction.merchant} </p>
         <b>{moneyFormatter.format(transaction.amount)}</b>
         <p className="RampText--hushed RampText--s">
-          {transaction.employee.firstName} {transaction.employee.lastName} - {transaction.date}
+          {transaction.employee.firstName} {transaction.employee.lastName} -{" "}
+          {transaction.date}
         </p>
       </div>
       <InputCheckbox
@@ -26,16 +29,16 @@ export const TransactionPane: TransactionPaneComponent = ({
           await consumerSetTransactionApproval({
             transactionId: transaction.id,
             newValue,
-          })
-
-          setApproved(newValue)
+          });
+          await clearCache(); // when changing employees, we want to clear cache allowing for the most accurate data retrieval
+          setApproved(newValue);
         }}
       />
     </div>
-  )
-}
+  );
+};
 
 const moneyFormatter = new Intl.NumberFormat("en-US", {
   style: "currency",
   currency: "USD",
-})
+});
